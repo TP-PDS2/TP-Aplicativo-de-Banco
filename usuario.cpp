f8c60c94f85b46a3d40b6273fadebc5886d21818
anabeatriz-0702 <148170135+anabeatriz-0702@users.noreply.github.com>
2023-10-30 19:31:31 -0300

    Update usuario.cpp

--- usuario.cpp
+++ usuario.cpp
@@ -1,1 +1,20 @@
+#include "usuario.h"
 
+using namespace std;
+
+void insert(const string& usuario_,
+              const string& nome,
+              const string& senha){ //INSERE UM USUÁRIO
+  
+}
+
+  
+
+void remove(const string& usuario_){  //REMOVE UM USUÁRIO
+  
+}
+ 
+
+bool senha_valida(const string& senha) const{//uma verificação de senhas válidas?
+  
+}
