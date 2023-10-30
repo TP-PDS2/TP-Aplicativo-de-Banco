add87fd3477e995b3badd17a1ecec438464a9ad3
isaac02901 <128757935+isaac02901@users.noreply.github.com>
2023-10-30 10:32:19 -0300

    Create usuario.h

---
+++ usuario.h
@@ -0,0 +1,28 @@
+#include <map>
+#include <string>
+
+using std::map;
+using std::string;
+
+class Usuário{
+  public:
+  void insert(const string& usuario_, //CPF?
+              const string& nome,
+              const string& senha);
+  //INSERE UM USUÁRIO
+
+   void remove(const usuario_& url);
+  //REMOVE UM USUÁRIO
+
+  private:
+  bool senha_valida(const string& senha) const;//uma verificação de senhas válidas?
+  private:
+  struct Usuario {
+    string usuario_;  // CPF do usuario.
+    string senha;  // senha do usuario.  
+  };
+ map<string, Usuario> senha_;
+};
+
+  
+}
