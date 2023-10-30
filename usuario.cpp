ee769326552a29b1671d738f1b30e7ba03602258
anabeatriz-0702 <148170135+anabeatriz-0702@users.noreply.github.com>
2023-10-30 19:39:08 -0300

    Update usuario.cpp

--- usuario.cpp
+++ usuario.cpp
@@ -6,6 +6,15 @@ void insert(const string& usuario_,
               const string& nome,
               const string& senha){ //INSERE UM USUÁRIO
   
+  if (senha_valida(senha)) {
+    auto it = senha.find(usuario_);
+    if (it == senha.end()) {
+      Usuario usuario_;
+      usuario_.nome = nome;
+      usuario_.senha = senha;
+      senha[usuario] = usuario_;
+    }
+  }
 }
 
   
