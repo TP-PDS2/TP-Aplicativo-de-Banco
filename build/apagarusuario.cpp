9c5400de3ca7933cf8276e41abccc0c2ae5897cc
Ana Beatriz <148170135+anabeatriz-0702@users.noreply.github.com>
2023-10-31 20:55:50 -0300

    Delete build/apagarusuario.cpp

--- build/apagarusuario.cpp
+++
@@ -1,9 +0,0 @@
-#include "apagarusuario.h"
-#include "usuario.h"
-
-void apagarUsuario(const std::string& cpf) {
-    auto it = usuarios.find(cpf);
-    if (it != usuarios.end()) {
-        usuarios.erase(it);
-    }
-}
