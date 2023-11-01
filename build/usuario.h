1a236a3cf7dda2fd0612c1527b314d4ae4675dc1
isaac02901 <128757935+isaac02901@users.noreply.github.com>
2023-10-31 21:09:19 -0300

    Update usuario.h

--- build/usuario.h
+++ build/usuario.h
@@ -15,6 +15,10 @@ class Usuário{
    void remove(const string& usuario_);
   //REMOVE UM USUÁRIO
 
+  bool redefinirsenha(const std::string& usuario);
+  //REDEFINE A SENHA DO USUÁRIO ATRAVÉS DE UM TOKEN DE CONFIRMAÇÃO
+  //(essa função ainda não pode ser implementada pois depende de estruturas que só serão criadas dentro de outras classes)
+
   private:
   bool senha_valida(const string& senha) const;//uma verificação de senhas válidas?
   
