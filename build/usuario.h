3c2d236b862b34a246b8bfeaf19c5312ac2a9d80
isaac02901 <128757935+isaac02901@users.noreply.github.com>
2023-10-31 21:40:03 -0300

    Update usuario.h

--- build/usuario.h
+++ build/usuario.h
@@ -20,7 +20,11 @@ class Usuário{
   //(essa função ainda não pode ser implementada pois depende de estruturas que só serão criadas dentro de outras classes)
 
   private:
-  bool senha_valida(const string& senha) const;//uma verificação de senhas válidas?
+  bool senha_valida(const string& senha) const;//VERIFICA SE A SENHA ATENDE AOS REQUISITOS
+  //entre 8 e 30 caracteres
+  //possui pelo menos um caractere maiusculo
+  //possui pelo menos um número
+  //possui pelo menos um caractere especial
   
   private:
   struct Usuario {
