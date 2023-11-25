baf0ba2e3c3d0510da748b270de0ca382768fb55
Ana Beatriz <148170135+anabeatriz-0702@users.noreply.github.com>
2023-11-25 15:27:32 -0300

    Create Emprestimo.cpp

---
+++ build/Emprestimo.cpp
@@ -0,0 +1,15 @@
+#include "Emprestimo.hpp"
+
+double Emprestimo::calcularJuros() {
+    // Lógica para calcular juros do empréstimo
+    return 0.0; // Exemplo: retorna 0
+}
+
+double Emprestimo::calcularValorParcela() {
+    // Lógica para calcular valor da parcela do empréstimo
+    return 0.0; // Exemplo: retorna 0
+}
+
+void Emprestimo::confirmarSolicitacao() {
+    // Lógica para confirmar a solicitação do empréstimo
+}
