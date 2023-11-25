5766c740ddd6be2c11180a4e4d73a2322c36a600
Ana Beatriz <148170135+anabeatriz-0702@users.noreply.github.com>
2023-11-25 15:25:01 -0300

    Create Saldo.hpp

---
+++ build/Saldo.hpp
@@ -0,0 +1,16 @@
+#ifndef SALDO_HPP
+#define SALDO_HPP
+
+class Saldo {
+private:
+    double saldo;
+
+public:
+    // Getter para saldo
+    double getSaldo() const;
+
+    // Setter para saldo
+    void setSaldo(double novoSaldo);
+};
+
+#endif
