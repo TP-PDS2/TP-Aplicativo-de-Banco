941d9b2ac8d530d5859fbb6140eda1e68fa055ca
Ana Beatriz <148170135+anabeatriz-0702@users.noreply.github.com>
2023-11-25 15:23:43 -0300

    Criação Transferencia.cpp

---
+++ build/Transferencia.cpp
@@ -0,0 +1,13 @@
+#include "Transferencia.hpp"
+
+void Transferencia::realizarTransferencia(int idContaOrigem, int idContaDestino, double valor) {
+    // Lógica para realizar transferência entre contas
+}
+
+void Transferencia::notificarErro() {
+    // Lógica para notificar erro na transferência
+}
+
+void Transferencia::notificarTransf() {
+    // Lógica para notificar sobre a transferência
+}
