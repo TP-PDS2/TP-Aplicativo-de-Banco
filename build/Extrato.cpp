25ddcbf62314ca40f62713e3767ef5e3a53bd560
Ana Beatriz <148170135+anabeatriz-0702@users.noreply.github.com>
2023-11-25 15:26:25 -0300

    Create Extrato.cpp

---
+++ build/Extrato.cpp
@@ -0,0 +1,13 @@
+#include "Extrato.hpp"
+
+void Extrato::informarExtratoAnual(int idConta) {
+    // Lógica para exibir extrato anual
+}
+
+void Extrato::informarExtratoMensal(int idConta) {
+    // Lógica para exibir extrato mensal
+}
+
+void Extrato::exportarExtratoDigital(int idConta) {
+    // Lógica para exportar extrato digital
+}
