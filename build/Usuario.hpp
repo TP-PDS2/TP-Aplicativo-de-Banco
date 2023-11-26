beb99a63b0ee5d805d2a4f8625531fd4ae2bff40
arthurzinnolasco <148912606+arthurzinnolasco@users.noreply.github.com>
2023-11-26 17:01:54 -0300

    Update Usuario.hpp (novo usuário e atribuições ao num de cc)
    fiz um void para adicionar novos usuários e um conjunto para armazenar o número das contas correntes associados a cada novo usuário, assim vamos poder tratar casos em que a geração de usuário nos de número de contas iguais.

--- build/Usuario.hpp
+++ build/Usuario.hpp
@@ -14,6 +14,9 @@ public:
     void remove(const std::string& usuario_);
 
     bool redefinirSenha(const std::string& usuario_);
+
+// Novo método para criar um novo usuário
+    void criarNovoUsuario();
     
 private:
     bool senhaValida(const std::string& senha_) const;
@@ -22,8 +25,18 @@ private:
     struct DadosUsuario_ {
         std::string usuario_;
         std::string senha_;
-    };
+        std::string nome_;
+        std::string endereco_;
+        std::string cpf_;
+        int anoNascimento_;
+        std::string numeroContaCorrente_; // Adicionando o número de conta corrente
+     };
+
     std::map<std::string, DadosUsuario_> usuariosRegistrados_;
+    std::set<std::string> numerosContaCorrenteGerados_; // Conjunto para armazenar números de conta corrente gerados
+
+    // Função auxiliar para gerar número de conta corrente aleatório
+    std::string gerarNumeroContaCorrente() const;
 };
 
 #endif // USUARIO_HPP
