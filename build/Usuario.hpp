b043bd486ac5a3ca5f7f2ba7101b16c08665c19a
arthurzinnolasco <148912606+arthurzinnolasco@users.noreply.github.com>
2023-11-26 17:30:59 -0300

    Update Usuario.hpp
    att do void que não estava sendo utilizado

--- build/Usuario.hpp
+++ build/Usuario.hpp
@@ -2,22 +2,22 @@
 #define USUARIO_HPP
 
 #include <map>
+#include <set>
 #include <string>
 #include <cctype>
+#include <iostream>
 
 class Usuario {
 public:
-    void insert(const std::string& usuario_,
-                const std::string& nome_,
-                const std::string& senha_);
+    void insert(const std::string& usuario_, const std::string& senha_, const std::string& opcao_);
 
     void remove(const std::string& usuario_);
 
     bool redefinirSenha(const std::string& usuario_);
 
-// Novo método para criar um novo usuário
+    // Novo método para criar um novo usuário
     void criarNovoUsuario();
-    
+
 private:
     bool senhaValida(const std::string& senha_) const;
 
@@ -25,15 +25,18 @@ private:
     struct DadosUsuario_ {
         std::string usuario_;
         std::string senha_;
+        std::string opcao_;
         std::string nome_;
         std::string endereco_;
         std::string cpf_;
         int anoNascimento_;
         std::string numeroContaCorrente_; // Adicionando o número de conta corrente
-     };
+    };
 
     std::map<std::string, DadosUsuario_> usuariosRegistrados_;
     std::set<std::string> numerosContaCorrenteGerados_; // Conjunto para armazenar números de conta corrente gerados
+
+    int tentativasSenha_;
 
     // Função auxiliar para gerar número de conta corrente aleatório
     std::string gerarNumeroContaCorrente() const;
