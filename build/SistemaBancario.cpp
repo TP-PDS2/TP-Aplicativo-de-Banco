ac48c8952560ffb47ce5613f46812f9407d80772
arthurzinnolasco <148912606+arthurzinnolasco@users.noreply.github.com>
2023-11-26 19:07:15 -0300

    Update SistemaBancario.cpp
    escopo da classe. temos aqui a integração entre Usuario e SistemaBancario para acesso ao sistema. Em breve vou fazer um main para podermos chamar esses parâmetros

--- build/SistemaBancario.cpp
+++ build/SistemaBancario.cpp
@@ -1,1 +1,29 @@
+// SistemaBancario.cpp
 
+#include "SistemaBancario.hpp"
+#include <iostream>
+
+void SistemaBancario::realizarLogin() {
+    std::string usuario, senha;
+
+    std::cout << "===== LOGIN =====" << std::endl;
+    std::cout << "Usuário: ";
+    std::cin >> usuario;
+
+    std::cout << "Senha: ";
+    std::cin >> senha;
+
+    // Verificar se o usuário existe e a senha está correta
+    if (Usuario::loginValido(usuario, senha)) {
+        usuarioAtual = Usuario::getUsuario(usuario);
+        std::cout << "Login bem-sucedido. Bem-vindo, " << usuarioAtual.obterNome() << "!" << std::endl;
+        exibirMenuUsuarioLogado();
+    } else {
+        std::cout << "Login falhou. Usuário ou senha incorretos." << std::endl;
+    }
+}
+
+void SistemaBancario::realizarCadastroUsuario() {
+    Usuario novoUsuario;
+    novoUsuario.criarNovoUsuario();
+}
