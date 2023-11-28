4ae86a9b20a7e3575d78e9b0bd39b33242208b5a
arthurzinnolasco <148912606+arthurzinnolasco@users.noreply.github.com>
2023-11-27 21:12:36 -0300

    Update Saldo.cpp

--- src/Saldo.cpp
+++ src/Saldo.cpp
@@ -1,11 +1,20 @@
 #include "Saldo.hpp"
+#include <iomanip>
 
-Saldo::Saldo() : saldo_(0.0) {}
-
-void Saldo::adicionarSaldo(double valor) {
-    saldo_ += valor;
-}
+Saldo::Saldo() : saldo(0.0) {}
 
 double Saldo::obterSaldo() const {
-    return saldo_;
+    return saldo;
+}
+
+void Saldo::adicionarSaldo(double valor) {
+    saldo += valor;
+}
+
+bool Saldo::diminuirSaldo(double valor) {
+    if (valor > saldo) {
+        return false; // Não há saldo suficiente para a operação
+    }
+    saldo -= valor;
+    return true;
 }
