dfccccc54a4d0d12df27d7a743530453d0b24cca
arthurzinnolasco <148912606+arthurzinnolasco@users.noreply.github.com>
2023-11-28 22:15:54 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
+#include <iostream>
+#include "Usuario.hpp"
 #include "SistemaBancario.hpp"
 
+using namespace std;
+
 int main() {
+
     SistemaBancario sistema;
     sistema.executarAplicativo();
 
