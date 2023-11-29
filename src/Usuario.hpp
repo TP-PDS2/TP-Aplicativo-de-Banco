ca590f2602f5ff2c5f518c997bfa37ceaa5576ef
arthurzinnolasco <148912606+arthurzinnolasco@users.noreply.github.com>
2023-11-28 22:17:17 -0300

    Update Usuario.hpp

--- src/Usuario.hpp
+++ src/Usuario.hpp
@@ -1,56 +1,39 @@
 #ifndef USUARIO_HPP
 #define USUARIO_HPP
 
-#include <map>
-#include <set>
 #include <string>
-#include <cctype>
-#include "SistemaBancario.hpp"
-#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
 #include "Saldo.hpp"
+
+
+struct DadosUsuario {
+    std::string nome;
+    std::string senha;
+    std::string dataNascimento;
+    std::string endereco;
+    std::string cpf;
+    std::string numeroContaCorrente;
+};
 
 class Usuario {
 public:
-    void insert(const std::string& usuario_, const std::string& senha_, const std::string& opcao_);
-
-    void remove(const std::string& usuario_);
-
-    bool redefinirSenha(const std::string& usuario_);
-
-    // Novo método para criar um novo usuário
-    void criarNovoUsuario();
-   void adicionarSaldo(double valor);
-   double obterSaldo() const;
-   bool fazerLogin();
-   void realizarOperacoesAposLogin();
-   void fazerLogout();
-   void mostrarInformacoesUsuario();
-   bool excluirConta();
-
-private:
-    std::string usuarioAtual_;
-    bool senhaValida(const std::string& senha_) const;
-
-private:
-    struct DadosUsuario_ {
-        std::string usuario_;
-        std::string senha_;
-        std::string opcao_;
-        std::string nome_;
-        std::string endereco_;
-        std::string cpf_;
-        int anoNascimento_;
-        std::string numeroContaCorrente_; // Adicionando o número de conta corrente
-    };
-    Saldo saldo;
-
-    std::map<std::string, DadosUsuario_> usuariosRegistrados_;
-    std::set<std::string> numerosContaCorrenteGerados_; // Conjunto para armazenar números de conta corrente gerados
-
-    int tentativasSenha_;
-
-    // Função auxiliar para gerar número de conta corrente aleatório
+    // Função para criar um novo usuário
+    void criarNovoUsuario(const std::string& nomeArquivo = "usuarios.txt");
     std::string gerarNumeroContaCorrente();
+    void salvarUsuariosEmArquivo(const std::string& nomeArquivo = "usuarios.txt") const;
+    void carregarUsuariosDeArquivo(const std::string& nomeArquivo = "usuarios.txt");
+    bool usuarioExiste(const std::string& nome) const;
+    bool fazerLogin(const std::string& cpf, const std::string& senha) const;
+    double obterSaldo() const;
+
+private:
+    // Vetor para armazenar os usuários
+    std::vector<DadosUsuario> usuarios;
+    bool senhaValida(const std::string& senha_) const;
+    std::set<std::string> numerosContaCorrenteGerados_;
+    Saldo saldo;
 };
 
-#endif
+#endif // USUARIO_HPP
