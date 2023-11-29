f7fa1d2462b44ddfd1f8043484df0105fb2d3e1f
arthurzinnolasco <148912606+arthurzinnolasco@users.noreply.github.com>
2023-11-29 17:58:35 -0300

    Create Desenvolvedor.hpp

---
+++ src/Desenvolvedor.hpp
@@ -0,0 +1,11 @@
+#ifndef DESENVOLVEDOR_HPP
+#define DESENVOLVEDOR_HPP
+
+#include "Usuario.hpp"
+
+class Desenvolvedor {
+public:
+    void listarUsuarios(const std::vector<DadosUsuario>& usuarios);
+};
+
+#endif
