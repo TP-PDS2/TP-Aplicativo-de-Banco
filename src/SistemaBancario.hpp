9f980a5b220088e10123d6449ee937ae088619a8
arthurzinnolasco <148912606+arthurzinnolasco@users.noreply.github.com>
2023-11-28 22:17:52 -0300

    Update SistemaBancario.hpp

--- src/SistemaBancario.hpp
+++ src/SistemaBancario.hpp
@@ -1,9 +1,21 @@
 #ifndef SISTEMABANCARIO_HPP
 #define SISTEMABANCARIO_HPP
 
+#include "Usuario.hpp"
+
 class SistemaBancario {
 public:
     void executarAplicativo();
+    SistemaBancario();
+
+private:
+    static void exibirMensagemBoasVindas();
+    void cadastrarNovoUsuario();
+    void listarCPFsCadastrados();
+    void realizarOperacoesAposLogin();
+    std::vector<DadosUsuario> usuarios;
+
+    int opcao; 
 };
 
-#endif // SISTEMABANCARIO_HPP
+#endif
