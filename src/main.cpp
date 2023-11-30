86a752a94d72d72b243bbf913856b5d71242b689
arthurzinnolasco <148912606+arthurzinnolasco@users.noreply.github.com>
2023-11-29 21:25:38 -0300

    Update main.cpp
    adicionei um usuario de adm para ter que parar de ficar criando usuario em todos os testes

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "SistemaBancario.hpp"
+#include "Usuario.hpp"
 
 using namespace std;
 
 int main()
 {
+    Usuario usuario;
+    usuario.adicionarUsuarioAdministrador();
     SistemaBancario sistema;
     sistema.executarAplicativo();
     
