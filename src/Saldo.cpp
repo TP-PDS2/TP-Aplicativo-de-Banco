fb8677a5083c3d0960dfc97a14c0f013138bf905
Ana Beatriz <148170135+anabeatriz-0702@users.noreply.github.com>
2023-12-04 18:50:12 -0300

    Conserto na atribuição do saldo Saldo.cpp

--- src/Saldo.cpp
+++ src/Saldo.cpp
@@ -1,21 +1,20 @@
 #include "Saldo.hpp"
-#include <iomanip>
 #include <cmath>
 
 Saldo::Saldo() : saldo(0.0) {}
 
 double Saldo::obterSaldo() const {
-    return (fabs(saldo));
+    return saldo;
 }
 
 void Saldo::adicionarSaldo(double valor) {
-    saldo += (fabs(valor));
+    saldo += valor;
 }
 
 bool Saldo::diminuirSaldo(double valor) {
-    if ((fabs(valor)) > saldo) {
+    if (valor > saldo) {
         return false; // Não há saldo suficiente para a operação
     }
-    saldo -= (fabs(valor));
+    saldo -= valor;
     return true;
 }
