dec099f5b8a7b5d9796039b1816e22909ec8112c
Ana Beatriz <148170135+anabeatriz-0702@users.noreply.github.com>
2023-12-04 18:49:09 -0300

    Conserto na atribuição do saldo Usuario.hpp

--- src/Usuario.hpp
+++ src/Usuario.hpp
@@ -30,10 +30,23 @@ public:
     void adicionarSaldo(double valor);
     bool diminuirSaldo(double valor);
     bool validarDataNascimento(const std::string& data) const;
+
+    Saldo& getSaldo(){
+       return saldo;
+    }
+
+    const Saldo& getSaldo() const{
+      return saldo;
+    }
     
+    void setSaldo(const Saldo& novoSaldo){
+      saldo = novoSaldo;
+    }
+
 private:
     // Vetor para armazenar os usuários
     static std::vector<DadosUsuario> usuarios;
+    Saldo saldo;
     bool senhaValida(const std::string& senha_) const;
     std::set<std::string> numerosContaCorrenteGerados_;
 };
